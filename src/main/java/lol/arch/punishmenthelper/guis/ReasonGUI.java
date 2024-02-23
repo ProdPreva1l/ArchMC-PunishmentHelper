e2402a5f5e4d0827854619dadad99192e4045ef8
ProdPreva1l <prod.preva1l@gmail.com>
2024-02-23 19:36:57 +1100

    fixed pagination, slight refactoring

--- src/main/java/lol/arch/punishmenthelper/guis/ReasonGUI.java
+++ src/main/java/lol/arch/punishmenthelper/guis/ReasonGUI.java
@@ -9,19 +9,19 @@ import lol.arch.punishmenthelper.utils.Sound;
 import lol.arch.punishmenthelper.utils.exceptions.GuiButtonException;
 import lol.arch.punishmenthelper.utils.gui.GuiButtonType;
 import lol.arch.punishmenthelper.utils.gui.GuiHelper;
-import lol.arch.punishmenthelper.utils.gui.PaginatedInv;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class ReasonGUI extends FastInv implements PaginatedInv {
+public class ReasonGUI extends FastInv {
     private final int page;
     private int index = 0;
     private final String target;
     private final FastInv previousGUI;
     private final PunishmentType punishmentType;
+    private final List<String> list;
 
     public ReasonGUI(PunishmentType punishmentType, String target, int page, FastInv previousGUI) {
         super(27, Menus.valueOf(punishmentType + "_GUI_TITLE").toFormattedString());
@@ -29,19 +29,20 @@ public class ReasonGUI extends FastInv implements PaginatedInv {
         this.target = target;
         this.previousGUI = previousGUI;
         this.punishmentType = punishmentType;
-
+        this.list = new ArrayList<>(getConfig().getConfiguration().getKeys(false));
 
         BasicConfig config = getConfig();
 
         try {
-            for (int i = 0; i < maxItemsPerPage(); i++) {
-                index = maxItemsPerPage() * page() + i;
-                if (index >= list().size()) break;
-                String reason = list().get(index);
+            int maxItemsPerPage = 18;
+            for (int i = 0; i < maxItemsPerPage; i++) {
+                index = maxItemsPerPage * page + i;
+                if (index >= list.size()) break;
+                String reason = list.get(index);
                 String itemName = config.getString(reason + ".gui-name");
                 List<String> itemLore = config.getStringList(reason + ".gui-lore");
                 Material material = Material.valueOf(config.getString(reason + ".gui-item"));
-                setItem(index, GuiHelper.constructButton(GuiButtonType.GENERIC, material, itemName, itemLore), e -> {
+                addItem(GuiHelper.constructButton(GuiButtonType.GENERIC, material, itemName, itemLore), e -> {
                     Sound.click((Player) e.getWhoClicked());
                     new FinalisePunishmentGUI(punishmentType, config, reason, target).open((Player) e.getWhoClicked());
                 });
@@ -66,12 +67,13 @@ public class ReasonGUI extends FastInv implements PaginatedInv {
 
         // Pagination Buttons
 
-        if (page() > 0) {
+        if (page > 0) {
             setItem(getInventory().getSize() - 9, GuiHelper.constructButton(GuiButtonType.PREVIOUS_PAGE), e -> {
+                Sound.click((Player) e.getWhoClicked());
                 new ReasonGUI(punishmentType, target, page - 1, previousGUI).open((Player) e.getWhoClicked());
             });
         }
-        if (list().size() >= index + 2) {
+        if (list.size() >= index + 2) {
             setItem(getInventory().getSize() - 1, GuiHelper.constructButton(GuiButtonType.NEXT_PAGE), e -> {
                 Sound.click((Player) e.getWhoClicked());
                 new ReasonGUI(punishmentType, target, page + 1, previousGUI).open((Player) e.getWhoClicked());
@@ -92,19 +94,4 @@ public class ReasonGUI extends FastInv implements PaginatedInv {
         }
     }
 
-
-    @Override
-    public int page() {
-        return page;
-    }
-
-    @Override
-    public int maxItemsPerPage() {
-        return 18;
-    }
-
-    @Override
-    public List<String> list() {
-        return new ArrayList<>(getConfig().getConfiguration().getKeys(false));
-    }
 }
--- src/main/java/lol/arch/punishmenthelper/utils/gui/PaginatedInv.java
+++
@@ -1,9 +0,0 @@
-package lol.arch.punishmenthelper.utils.gui;
-
-import java.util.List;
-
-public interface PaginatedInv {
-    int page();
-    int maxItemsPerPage();
-    List<String> list();
-}
